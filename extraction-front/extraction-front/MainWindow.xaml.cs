22fd52f04fd798a0f3cc2c90950b814e31aeb318
Rika Voice <skyvory@hotmail.com>
2017-07-08 16:15:16 +0800

    Multi page draft

--- extraction-front/extraction-front/MainWindow.xaml.cs
+++ extraction-front/extraction-front/MainWindow.xaml.cs
@@ -25,85 +25,34 @@ namespace extraction_front
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(packfilepathtext.Text) || string.IsNullOrWhiteSpace(keyfilepathtext.Text) || string.IsNullOrWhiteSpace(exefilepathtext.Text))
+            Main.Content = new Page1();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page2();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (toolselection.SelectedValue == null)
             {
-                MessageBox.Show("All fields are required to fill.");
                 return;
             }
-
-            if (RadioButton1.IsChecked == true)
-            {
-               //
-            }
-            else if (RadioButton2.IsChecked == true)
-            {
-                //
-            }
-
-            string args = "/C ..\\exfp3_v3.exe \"";
-            args += packfilepathtext.Text;
-            args += "\" \"";
-            args += keyfilepathtext.Text;
-            args += "\" \"";
-            args += exefilepathtext.Text;
-            args += "\"";
-
-            System.IO.Directory.CreateDirectory("extraction");
-            string workingdirectory = System.IO.Directory.GetCurrentDirectory() + "\\extraction";
-
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfo.WorkingDirectory = workingdirectory;
-            startInfo.FileName = "cmd.exe";
-
-            startInfo.Arguments = args;
-            process.StartInfo = startInfo;
-            process.Start();
-        }
-
-        private void packfilebutton_Click(object sender, RoutedEventArgs e)
-        {
-            Microsoft.Win32.OpenFileDialog filedialog = new Microsoft.Win32.OpenFileDialog();
-            filedialog.Filter = "Pack File (*.pack,*.packwhatever)|*.pack;*.packwhatever|All Files (*.*)|*.*";
-            filedialog.FilterIndex = 1;
-            filedialog.Multiselect = false;
-
-            if (filedialog.ShowDialog() == true)
-            {
-                string packfilename = filedialog.FileName;
-                packfilepathtext.Text = packfilename;
+            switch(toolselection.SelectedValue.ToString()) {
+                case "exfp3":
+                    Main.Content = new Page1();
+                    break;
+                case "exkifint":
+                    Main.Content = new Page2();
+                    break;
+                default:
+                    Main.Content = new Page1();
+                    break;
             }
         }
 
-        private void keyfilebutton_Click(object sender, RoutedEventArgs e)
-        {
-            Microsoft.Win32.OpenFileDialog filedialog = new Microsoft.Win32.OpenFileDialog();
-            filedialog.Filter = "Key File (*.fkey)|*.fkey|All Files (*.*)|*.*";
-            filedialog.FilterIndex = 1;
-            filedialog.Multiselect = false;
-
-            if (filedialog.ShowDialog() == true)
-            {
-                string filename = filedialog.FileName;
-                keyfilepathtext.Text = filename;
-            }
-        }
-
-        private void exefilebutton_Click(object sender, RoutedEventArgs e)
-        {
-            Microsoft.Win32.OpenFileDialog filedialog = new Microsoft.Win32.OpenFileDialog();
-            filedialog.Filter = "Executables (*.exe)|*.exe|All Files (*.*)|*.*";
-            filedialog.FilterIndex = 1;
-            filedialog.Multiselect = false;
-
-            if (filedialog.ShowDialog() == true)
-            {
-                string filename = filedialog.FileName;
-                exefilepathtext.Text = filename;
-            }
-        }
     }
 }
